c03ece4c6e7ba1b5c591f39a6c0516c03dfbfa81
Akhil K S <akhilshaji003@gmail.com>
2021-02-25 14:47:28 +0530

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Navbar,Nav} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+
+class Header extends Component{
+
+render(){
+    return(
+
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Covid-19 Tracker</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Link className="nav-link" to="/india">India</Link>
+      <Link className="nav-link" to="/world">World</Link>
+     
+    </Nav>
+    
+  </Navbar.Collapse>
+</Navbar>
+
+
+
+
+
+
+    )
+}
+
+
+
+}
+export default Header;
